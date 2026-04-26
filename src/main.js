69b3c92aafbe2a8c2bbc02b637a63fab952eb9fb
AppleMan456 <johanngao@gmail.com>
2026-04-26 23:23:30 +0100

    Refactor Example class in main.js

--- src/main.js
+++ src/main.js
@@ -92,7 +92,7 @@
                 const upbg = this.add.graphics();
                 upbg.fillStyle(0x000000, 1);
                 upbg.setInteractive();
-                upbg.setdepth(9999);
+                upbg.setDepth(9999);
                 const rupbg = () => {
                     boundup = upgradetext.getBounds();
                     upbg.fillRect(
