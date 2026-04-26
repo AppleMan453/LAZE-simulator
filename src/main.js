f073dba7b0e48c463a7e396c630cc9220e5d9b19
AppleMan456 <johanngao@gmail.com>
2026-04-26 22:46:27 +0100

    Refactor main.js for game initialization and upgrades

--- src/main.js
+++ src/main.js
@@ -182,7 +182,7 @@
         parent: 'game-container',
         
         scale: {
-            mode: Phaser.Scale.RESIZE,
+            mode: Phaser.Scale.FIT,
             autoCenter: Phaser.Scale.CENTER_BOTH,
             width: isMobile ? 854 : 1280,
             height: isMobile ? 480 : 720,  
